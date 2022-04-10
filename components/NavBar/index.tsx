4c184b493375106f03b8cbd042cdba9d1737b0bd
Lara <sabatinilara@gmail.com>
2022-04-10 20:11:12 -0300

    Functional: added event to connect to wallet

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -12,25 +12,11 @@ import {
 
 function NavBar() {
   const { plainContent } = useContext(PlainContentContext)
-  const { setIsConnected, isConnected } = useContext(AppContext)
+  const { isConnected, connectToMetamask } = useContext(AppContext)
 
   const connectToWallet = () => {
-    if (window.ethereum) {
-      window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then((res: any) => {
-          // Chequear que el usuario este conectado a ropsten, sino no dejarlo avanzar
-          // Si no esta conectado a ropsten mostrar un boton para que cambie a esa red
-
-          // Mostrar el balance del token $QUIZ
-          console.log("respuesta conexion", res)
-          setIsConnected(true)
-        })
-    } else {
-      // eslint-disable-next-line no-alert
-      alert("Please install metamask extension to continue")
-    }
-    // setIsConnected(!isConnected)
+    connectToMetamask()
+    // Mostrar el balance del token $QUIZ
   }
 
   return (
--- contexts/appContext.tsx
+++ contexts/appContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import { createContext, useState } from "react"
 
 export const AppContext = createContext({
@@ -5,6 +6,9 @@ export const AppContext = createContext({
   setPageState: null,
   isConnected: null,
   setIsConnected: null,
+  connectToMetamask: null,
+  connectedToRopsten: null,
+  setConnectedToRopsten: null,
 })
 
 function AppProvider({ children }) {
@@ -12,6 +16,42 @@ function AppProvider({ children }) {
   const [pageState, setPageState] = useState<string>("")
   // State to see if the wallet is connected or not to the wallet
   const [isConnected, setIsConnected] = useState<boolean>(false)
+  // State to see if the wallet is connected to the right network
+  const [connectedToRopsten, setConnectedToRopsten] = useState<boolean>(false)
+
+  const connectToMetamask = async () => {
+    const { ethereum } = window
+
+    const chainId = await ethereum.request({ method: "eth_chainId" })
+    handleChainChanged(chainId)
+    ethereum.on("chainChanged", handleChainChanged)
+
+    function handleChainChanged(_chainId: any) {
+      // DETECCION DE LA RED => TIENE QUE SER 0x3 la ropsten
+      // Hacer condicional de una sola linea
+      // eslint-disable-next-line no-console
+      console.log("Red conectada", _chainId)
+      if (_chainId === "0x3") {
+        setConnectedToRopsten(true)
+      } else {
+        setConnectedToRopsten(false)
+        // Mostrar boton para cambiar de red
+      }
+    }
+
+    if (typeof window !== undefined) {
+      if (ethereum) {
+        ethereum.request({ method: "eth_requestAccounts" }).then((res: any) => {
+          // eslint-disable-next-line no-console
+          console.log("respuesta conexion", res)
+          setIsConnected(true)
+        })
+      } else {
+        // eslint-disable-next-line no-alert
+        alert("Please install metamask extension to continue")
+      }
+    }
+  }
 
   return (
     <AppContext.Provider
@@ -21,6 +61,9 @@ function AppProvider({ children }) {
         setPageState,
         isConnected,
         setIsConnected,
+        connectToMetamask,
+        connectedToRopsten,
+        setConnectedToRopsten,
       }}
     >
       {children}
