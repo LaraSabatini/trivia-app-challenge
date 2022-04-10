6ec5ca46ae5a96a1944395e71833b797cf54d412
Lara <sabatinilara@gmail.com>
2022-04-10 14:48:01 -0300

    Added media queries

--- components/NavBar/styles.tsx
+++ components/NavBar/styles.tsx
@@ -6,12 +6,19 @@ const Navigation = styled.nav`
     gap: 30px;
     align-items: flex-end;
     margin: 25px 25px;
+    width: 100%;
+    @media screen and (max-width: 500px) {
+        gap: 10px;
+    }
 `
 
 const Title = styled.h1`
     color: ${theme.colors.gray};
     font-size: ${theme.fontSizes.title};
     margin: 0;
+    @media screen and (max-width: 500px) {
+        font-size: 16px;
+    }
 `
 
 const ImageContainer = styled.div`
@@ -22,10 +29,13 @@ const ImageContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-
     img {
         width: 100%;
         height: 100%;
+    }
+    @media screen and (max-width: 500px) {
+        width: 50px;
+        height: 50px;
     }
 `
 
