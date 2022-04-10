b821e40782c92bc310cf33588cc7f84b777edaef
Lara <sabatinilara@gmail.com>
2022-04-10 13:48:57 -0300

    Created theme

---
+++ theme/index.ts
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+        primary: "#282A36",
+        white: "#F8F8F2",
+        gray: "#E6E6E6",
+        orange: "#E99859",
+        orange_background: "#FFB86C",
+        orange_light: "#ECC19F",
+    },
+    fontSizes: {
+        title: "42px",
+    }
+}
