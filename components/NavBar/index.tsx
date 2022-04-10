a2b5b19002ff4e04869fba5c86ea4cc57dc2570b
Lara <sabatinilara@gmail.com>
2022-04-10 19:08:57 -0300

    Added funtion to connect to metamask

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -15,8 +15,22 @@ function NavBar() {
   const { setIsConnected, isConnected } = useContext(AppContext)
 
   const connectToWallet = () => {
-    console.log("connecting")
-    setIsConnected(!isConnected)
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res: any) => {
+          // Chequear que el usuario este conectado a ropsten, sino no dejarlo avanzar
+          // Si no esta conectado a ropsten mostrar un boton para que cambie a esa red
+
+          // Mostrar el balance del token $QUIZ
+          console.log("respuesta conexion", res)
+          setIsConnected(true)
+        })
+    } else {
+      // eslint-disable-next-line no-alert
+      alert("Please install metamask extension to continue")
+    }
+    // setIsConnected(!isConnected)
   }
 
   return (
@@ -28,7 +42,7 @@ function NavBar() {
         <Title>{plainContent?.title}</Title>
       </LogoContainer>
       <Connect onClick={connectToWallet}>
-        <p>{!isConnected ? "Connect" : "Disconnect"}</p>
+        <p>{!isConnected && "Connect"}</p>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
           alt="Connect"
