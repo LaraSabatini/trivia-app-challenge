b604b7ea1ccc81172fc2fc4885a3fe377eb66d85
Lara <sabatinilara@gmail.com>
2022-04-13 15:41:01 -0300

    added start survey action

--- components/AppContainer/index.tsx
+++ components/AppContainer/index.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useContext } from "react"
 import { PlainContentContext } from "contexts/plainContentContext"
+import { SurveyContext } from "contexts/surveyContext"
 import { AppContext } from "contexts/appContext"
 import NavBar from "components/NavBar"
+import QuestionContainer from "components/QuestionContainer"
 import getPlainContent from "services/getPlainContent.service"
 import StartButton from "./styles"
 
 function MainContainer() {
   const { setPlainContent } = useContext(PlainContentContext)
   const { isConnected } = useContext(AppContext)
+  const { setSurveyView, surveyView } = useContext(SurveyContext)
 
-  const renderingStart = async () => {
-    const content = await getPlainContent()
+  const renderingStart = () => {
+    const content = getPlainContent()
     setPlainContent(content)
+    setSurveyView(false)
   }
 
   useEffect(() => {
@@ -19,18 +23,29 @@ function MainContainer() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const startSurvey = () => {
+    if (isConnected.connectedToMetamastk && isConnected.connectedToNetwork) {
+      setSurveyView(true)
+    }
+  }
+
   return (
     <>
       <NavBar />
-      <StartButton
-        active={
-          isConnected.connectedToMetamastk && isConnected.connectedToNetwork
-        }
-      >
-        {isConnected.connectedToMetamastk && isConnected.connectedToNetwork
-          ? "Start survey"
-          : "Connect to start survey"}
-      </StartButton>
+      {surveyView ? (
+        <QuestionContainer />
+      ) : (
+        <StartButton
+          active={
+            isConnected.connectedToMetamastk && isConnected.connectedToNetwork
+          }
+          onClick={startSurvey}
+        >
+          {isConnected.connectedToMetamastk && isConnected.connectedToNetwork
+            ? "Start survey"
+            : "Connect to start survey"}
+        </StartButton>
+      )}
     </>
   )
 }
