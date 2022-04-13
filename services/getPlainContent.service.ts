1a2f50557ed0282dfbaf657c36dd298c628dfcd3
Lara <sabatinilara@gmail.com>
2022-04-13 15:05:53 -0300

    changed content of plainContent

--- services/getPlainContent.service.ts
+++ services/getPlainContent.service.ts
@@ -1,66 +1,77 @@
-// import axios from "axios"
-
-// const apiURL = "https://ratherlabs-challenges.s3.sa-east-1.amazonaws.com/survey-sample.json"
-
-const getPlainContent = async () => {
-  // const axiosHeader = {
-  //     headers: {
-  //         "Content-Type": "application/json",
-  //     }
-  // }
-  // const res = await axios.get(apiURL, axiosHeader)
+const getPlainContent = () => {
   const res = {
     title: "Sample Survey",
     image: "https://48tools.com/wp-content/uploads/2015/09/shortlink.png",
     questions: [
       {
-        text: "Question1",
+        text: "What is the color of this fruit?",
         image:
           "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
-        lifetimeSeconds: 10,
+        lifetimeSeconds: 6,
+        id: 100,
         options: [
           {
-            text: "Opt1",
+            text: "Orange",
+            value: true,
+            id: 1,
           },
           {
-            text: "Opt2",
+            text: "Green",
+            value: false,
+            id: 2,
           },
           {
-            text: "Opt",
+            text: "Blue",
+            value: false,
+            id: 3,
           },
         ],
       },
       {
-        text: "Question2",
+        text: "What is the capital of turkey?",
         image:
           "https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg",
-        lifetimeSeconds: 5,
+        lifetimeSeconds: 6,
+        id: 101,
         options: [
           {
-            text: "Opt1",
+            text: "Kabul",
+            value: false,
+            id: 4,
           },
           {
-            text: "Opt2",
+            text: "Ankara",
+            value: true,
+            id: 5,
           },
           {
-            text: "Opt",
+            text: "Berlín",
+            value: false,
+            id: 6,
           },
         ],
       },
       {
-        text: "Pregunta 2",
+        text: "What is the world's largest country?",
         image:
           "https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg",
-        lifetimeSeconds: 5,
+        lifetimeSeconds: 6,
+        id: 102,
         options: [
           {
-            text: "Opt1",
+            text: "Russia",
+            value: true,
+            id: 7,
           },
           {
-            text: "Opt2",
+            text: "Canada",
+            value: false,
+            id: 8,
           },
           {
-            text: "Opt",
+            text: "China",
+            value: false,
+            id: 9,
           },
         ],
       },
