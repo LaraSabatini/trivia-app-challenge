ad25d4dd9a92148fc67255ab3e718788359b3a25
Lara <sabatinilara@gmail.com>
2022-04-15 18:15:43 -0300

    created interfaces

---
+++ interfaces/surveyInterfaces.ts
@@ -0,0 +1,24 @@
+export interface QuestionsInterface {
+    text: string
+    image: string
+    lifetimeSeconds: number
+    id: number
+    options: {
+        text: string
+        value: boolean
+        id: number
+    }[]
+}
+
+export interface AnswersSelectedInterface {
+    question_id: number
+    answer_id: number
+    value: boolean
+    text: string
+}
+
+export interface AnswerInterface {
+    value: boolean
+    text: string
+    id: number
+}
