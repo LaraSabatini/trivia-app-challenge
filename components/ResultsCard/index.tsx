65e4868e57bf618a54d56b1226fb720271de2053
Lara <sabatinilara@gmail.com>
2022-04-15 18:48:51 -0300

    added results view

--- components/ResultsCard/index.tsx
+++ components/ResultsCard/index.tsx
@@ -1,8 +1,12 @@
 import React, { useContext, useEffect } from 'react'
 import { SurveyContext } from "contexts/surveyContext"
+import { AppContext } from "contexts/appContext"
+import getBalance from "services/getBalance.service"
 import theme from "theme/index"
+import token from "tokens/QUIZ.json"
+import { QuestionsInterface, AnswersSelectedInterface } from "interfaces/surveyInterfaces"
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons"
-import { ResultsContainer, Content } from "./styles"
+import { ResultsContainer, Content, Send, Answer } from "./styles"
 import { Title } from "../QuestionContainer/styles"
 
 const Results = () => {
@@ -13,23 +17,14 @@ const Results = () => {
     setAnswersSelected,
   } = useContext(SurveyContext)
 
-  // se mapea questions para las preguntas
-  // se mapea answers selected para las respuestas
+  const {
+    account,
+  } = useContext(AppContext)
 
   const checkIfAllQuestionsHaveBeenAnswered = () => {
     if (questions.length !== answersSelected.length) {
-      const idsOfQuestions = questions.map((question: {text: string
-        image: string
-        lifetimeSeconds: number
-        id: number
-        options: {
-          text: string
-          value: boolean
-          id: number
-        }[]}) => question.id)
-        const idsOfAnswers = answersSelected.map((answer: {question_id: number
-          answer_id: number
-          value: boolean}) => answer.question_id)
+      const idsOfQuestions = questions.map((question: QuestionsInterface) => question.id)
+        const idsOfAnswers = answersSelected.map((answer: AnswersSelectedInterface) => answer.question_id)
         const answersToAdd = idsOfQuestions.filter((id: number) => !idsOfAnswers.includes(id))
 
         const newArrayOfAnswers = answersSelected
@@ -38,6 +33,7 @@ const Results = () => {
               question_id: answersToAdd[i],
               answer_id: 0,
               value: false,
+              text: "Without answer"
             })
         }
         setAnswersSelected(newArrayOfAnswers)
@@ -50,26 +46,29 @@ const Results = () => {
     }
   }, [checkResults])
 
+  const sendResults = async () => {
+    // console.log("enviando")
+    const res = await getBalance(token.address, account[0])
+    // setBalance(res.result)
+    console.log(res)
+    
+  }
+
+
   return (
     <ResultsContainer>
       <Title>Results:</Title>
          <Content>
-           {questions.map((question: {text: string
-              image: string
-              lifetimeSeconds: number
-              id: number
-              options: {
-                text: string
-                value: boolean
-                id: number
-              }[]}) => {
-                <>
-                  <p>
-                    {/* {answer.value ? <CheckCircleOutlined style={{color:`${theme.colors.green}`}} /> : <CloseCircleOutlined style={{color:`${theme.colors.red}`}} />} */}
-                  </p>
-                </>
-          })}
+           {answersSelected.map((answer: AnswersSelectedInterface) =>
+              <Answer>
+                {answer.value ? <CheckCircleOutlined style={{ color: `${theme.colors.green}` }} /> : <CloseCircleOutlined style={{ color: `${theme.colors.red}` }} />}
+                <p>
+                  {answer.text}
+                </p>
+              </Answer>
+            )}
          </Content>
+         <Send type="button" onClick={sendResults}>SUBMIT</Send>
     </ResultsContainer>
   )
 }
--- components/ResultsCard/styles.tsx
+++ components/ResultsCard/styles.tsx
@@ -7,15 +7,58 @@ const ResultsContainer = styled.div`
   border-radius: 10px;
   padding: 15px;
   margin: 0 auto;
+  box-shadow: 0px 4px 15px 5px rgba(236, 193, 159, 0.15);
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (max-width: ${theme.resolutions.tablet}) {
+    width: 40%;
+  }
+
+  @media screen and (max-width: ${theme.resolutions.mobile}) {
+    width: 60%;
+  }
 `
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  margin-top: 20px;
+
+  p {
+    font-size: ${theme.fontSizes.secondDesktop};
+    color: ${theme.colors.primary};
+    margin: 0;
+  }
+`
+
+const Answer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 15px 5px;
+`
+
+const Send = styled.button`
+  border: none;
+  outline: none;
+  background-color: ${theme.colors.orange};
+  color: ${theme.colors.primary};
+  font-family: ${theme.fontFamilies.primary};
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 10px;
+  border: 1px solid transparent;
+  &:hover {
+    border: 1px solid ${theme.colors.orange_background}
+  }
 `
 
 export {
     ResultsContainer,
     Content,
+    Send,
+    Answer
 }
