38c0f599d5797cd91a6f9407b7afffb9400828c3
Lara <sabatinilara@gmail.com>
2022-04-16 11:51:52 -0300

    this commit has no content

--- contexts/appContext.tsx
+++ contexts/appContext.tsx
@@ -68,7 +68,6 @@ function AppProvider({ children }) {
     const chainId = await ethereum.request({ method: "eth_chainId" })
 
     if (typeof window !== undefined && ethereum) {
-      // window.ethereum.enable()
       ethereum
         .request({ method: "eth_requestAccounts" })
         .then((accounts: any) => {
